fd1dbf6d431975e1a371e198f03ed1d82b538ec8
Neksov <n2v@list.ru>
2020-10-01 00:06:21 +0300

    add commit for code

--- js/main.js
+++ js/main.js
@@ -13,20 +13,20 @@ overlay.classList.add('overlay'); //присваевам класc
 document.body.insertAdjacentElement('beforeend', overlay);
 
 const openMenu = () => {
-  catalog.classList.add("open");
-  overlay.classList.add("active");
+  catalog.classList.add("open"); //добавляем класс 
+  overlay.classList.add("active"); //добавляем класс
 };
 const closeMenu = () => {
   closeSubMenu();
-  catalog.classList.remove("open");
+  catalog.classList.remove("open"); //удаляем класс
   overlay.classList.remove("active");
 };
 const openSubMenu = (event) => {
-  event.preventDefault(); //отменяет переход на другую страницу при нажатии на ссылку в меню
-  const target = event.target;
-  const itemList = target.closest(".catalog-list__item");
+  event.preventDefault(); //отменяет переход на другую страницу при нажатии на ссылку в меню event-обьект 
+  const target = event.target; //target свойство оебьекта event 
+  const itemList = target.closest(".catalog-list__item"); //метод closest берет элемент на который кликнул и по DOM дереву идет наверх, пока не встретит элемент с  тем селектором который мы напишем иначе null 
   if (itemList) {
-    subcatalogHeader.innerHTML = itemList.innerHTML; //забиарем с target и отдаем Header для добавелнеия имени у сабменю
+    subcatalogHeader.innerHTML = itemList.innerHTML; //забиарем и отдаем Header для добавелнеия имени у сабменю
     subCatalog.classList.add("subopen");
   }
 };
@@ -35,12 +35,14 @@ const closeSubMenu = () => {
   subCatalog.classList.remove("subopen");
 };
 
+//вызов метода
 btnBurger.addEventListener("click", openMenu);
 btnClose.addEventListener("click", closeMenu);
 overlay.addEventListener("click", closeMenu);
 catalog.addEventListener("click", openSubMenu);
 btnReturn.addEventListener("click", closeSubMenu);
 
+//закрытие меню по нажатию на ESC
 document.addEventListener("keydown", (event) => {
   if (event.code === "Escape") {
     closeMenu();
