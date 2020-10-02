23243b110aae219ede2b4af9ef0319305f94af3a
Neksov <n2v@list.ru>
2020-10-02 14:23:10 +0300

    add commit

--- js/generateGoodsPage.js
+++ js/generateGoodsPage.js
@@ -1,25 +1,22 @@
-import {
-  getData
-} from './getData.js'
-const wishList = ['idd021', 'idd054', 'idd023', 'idd078', 'idd099'];
+import { getData } from "./getData.js";
+const wishList = ["idd021", "idd054", "idd023", "idd078", "idd099"];
 
 const generateGoodsPage = () => {
-
-  const mainHeader = document.querySelector('.main-header');
-  const goodsList = document.querySelector('.goods-list');
+  const mainHeader = document.querySelector(".main-header");
+  const goodsList = document.querySelector(".goods-list");
 
   const generateCards = (data) => {
-    goodsList.textContent = '';
+    goodsList.textContent = "";
     data.forEach((item) => {
-
-      goodsList.insertAdjacentHTML(`afterbegin`,
+      goodsList.insertAdjacentHTML(
+        `afterbegin`,
         `<li class="goods-list__item">
         <a class="goods-item__link" href="card.html#${item.id}">
             <article class="goods-item">
                 <div class="goods-item__img">
-                    <img src=${item.img[0]}
-                        data-second-image=${item.img[1]} alt="${item.name}">
-                </div>
+                           <img src="https://www.ikea.com/ru/ru/images/products/fabler-byorn-myagkaya-igrushka-bezhevyy__0710165_PE727396_S5.JPG"
+                              data-second-image="https://www.ikea.com/ru/ru/images/products/fabler-byorn-myagkaya-igrushka-bezhevyy__0876876_PE611263_S5.JPG" alt="ФАБЛЕР БЬЁРН">
+                        </div>
                 <h3 class="goods-item__header">${item.name}</h3>
                 <p class="goods-item__description">${item.description}</p>
                 <p class="goods-item__price">
@@ -29,22 +26,26 @@ const generateGoodsPage = () => {
                 <button class="btn btn-add-card" aria-label="Добравить в корзину" data-idd="${item.id}"></button>
             </article>
         </a>
-    </li>`)
-    })
-  }
+    </li>`
+      );
+    });
+  };
 
-  if (location.pathname.includes('goods') && location.search) {
-    const search = decodeURI(location.search); //decodeURI декодирование из непонятных символов на латинские буквы 
-    const prop = search.split('=')[0].substring(1); // split разбивает/разделяем строку на на символ =, [0] получаем 0 элемент
-    const value = search.split('=')[1];
+  if (location.pathname.includes("goods") && location.search) {
+    const search = decodeURI(location.search); //decodeURI декодирование из непонятных символов на латинские буквы
+    const prop = search.split("=")[0].substring(1); // split разбивает/разделяем строку на на символ =, [0] получаем 0 элемент
+    const value = search.split("=")[1];
 
-    if (prop === 's') {
+    if (prop === "s") {
+      // запрос поиска
       getData.search(value, generateCards);
       mainHeader.textContent = `Поиск: ${value}`;
-    } else if (prop === 'wishlist') {
+    } else if (prop === "wishlist") {
+      // запрос списка желаний
       getData.wishList(wishList, generateCards); //callback функция
       mainHeader.textContent = `Список желаний`;
-    } else if (prop === 'cat' || prop === 'subcat') {
+    } else if (prop === "cat" || prop === "subcat") {
+      // запрос категорий
       getData.category(prop, value, generateCards);
       mainHeader.textContent = value;
     }
